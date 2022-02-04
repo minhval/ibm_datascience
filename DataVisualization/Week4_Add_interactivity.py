e44e5c8aab47c37498001e6d64990fe0375ef0e5
minhval <levuanhminh0307@gmail.com>
2022-02-04 20:33:28 +0100

    Push notebooks

---
+++ DataVisualization/Week4_Add_interactivity.py
@@ -0,0 +1,27 @@
+# Import required libraries
+import pandas as pd
+import plotly.graph_objects as go
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+from dash.dependencies import Input, Output
+
+# Read the airline data into pandas dataframe
+airline_data =  pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DV0101EN-SkillsNetwork/Data%20Files/airline_data.csv', 
+                            encoding = "ISO-8859-1",
+                            dtype={'Div1Airport': str, 'Div1TailNum': str, 
+                                   'Div2Airport': str, 'Div2TailNum': str})
+
+# Create a dash application
+app = dash.Dash(__name__)
+
+# Get the layout of the application and adjust it.
+# Create an outer division using html.Div and add title to the dashboard using html.H1 component
+# Add a html.Div and core input text component
+# Finally, add graph component.
+app.layout = html.Div(children=[html.H1(),                                html.Div(["Input Year", dcc.Input(),], 
+                                style={}),
+                                html.Br(),
+                                html.Br(),
+                                html.Div(),
+                                ])
---
+++ DataVisualization/int(True).py
@@ -0,0 +1,7 @@
+import numpy as np
+
+var = "01234567"
+
+a=np.array([0,1,0,1,0])
+b=np.array([1,0,1,0,1])
+print(a+b)
